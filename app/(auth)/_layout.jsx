be4cbe4f4ebdbb9caf85eb4ad45f691393817c43
Vedesh Padal <vpadal04@gmail.com>
2024-05-02 09:51:37 +0530

    signup and signin screens complete

--- app/(auth)/_layout.jsx
+++ app/(auth)/_layout.jsx
@@ -1,12 +1,24 @@
-import { View, Text } from 'react-native'
+import { View, Text, StatusBar } from 'react-native'
 import { Stack } from 'expo-router'
 
 const AuthLayout = () => {
   return (
     <>
       <Stack>
-
+        <Stack.Screen 
+          name='sign-in'
+          options={{
+            headerShown: false
+          }}
+        />
+        <Stack.Screen 
+          name='sign-up'
+          options={{
+            headerShown: false
+          }}
+        />
       </Stack>
+      <StatusBar backgroundColor='#161622' />
     </>
   )
 }
