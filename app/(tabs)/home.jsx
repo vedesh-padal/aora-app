52d535728454ba66f4b336f2649bddac4252711f
Vedesh Padal <vpadal04@gmail.com>
2024-05-04 16:15:51 +0530

    Home screen with videocards playing complete

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,32 +1,42 @@
-import { View, Text, FlatList, Image, RefreshControl } from 'react-native'
-import React, { useState } from 'react'
+import { View, Text, FlatList, Image, RefreshControl, Alert } from 'react-native'
+import React, { useEffect, useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { images } from '../../constants'
 import SearchInput from '../../components/SearchInput'
 import Trending from '../../components/Trending'
 import EmptyState from '../../components/EmptyState'
+import { getAllPosts, getLatestPosts } from '../../lib/appwrite'
+import useAppwrite from '../../lib/useAppwrite'
+import VideoCard from '../../components/VideoCard'
 
 const Home = () => {
+
+  const { data: posts, refetch } = useAppwrite(getAllPosts);
+  const { data: latestPosts } = useAppwrite(getLatestPosts);
 
   const [refreshing, setRefreshing] = useState(false);
 
   const onRefresh = async () => {
     setRefreshing(true);
     // re-call videos if any new appeared
+    await refetch();
     setRefreshing(false);
   }
+
+  // console.log(posts)  ;
 
   return (
     <SafeAreaView className='bg-primary h-full'>
       <FlatList 
-        data={[ { id: 1 }, { id: 2 }, { id: 3 } ]}
+        data={posts}
         // data={[]}
         keyExtractor={ (item) => item.$id }
         renderItem={ ({ item }) => (
-          <Text className='text-3xl text-white' key={item.id}>
-            {item.id}
-          </Text>
+          <VideoCard 
+            video={item}
+          />
         )}
+
         ListHeaderComponent={() => (
           <View className="my-6 px-4 space-y-6">
             <View className='justify-between items-start flex-row mb-6'>
@@ -54,7 +64,7 @@ const Home = () => {
                 Latest Videos
               </Text>
               <Trending 
-                posts={[ {id: 1}, { id: 2}, {id: 3}] ?? []}
+                posts={latestPosts ?? []}
               />
             </View>
           </View>
---
+++ components/VideoCard.jsx
@@ -0,0 +1,77 @@
+import { View, Text, Image, TouchableOpacity } from 'react-native'
+import React, { useState } from 'react'
+import { icons } from '../constants'
+import { ResizeMode, Video } from 'expo-av'
+
+const VideoCard = ({ video: { title, thumbnail, video, creator: { username, avatar }}}) => {
+
+  const [play, setPlay] = useState(false)
+
+  return (
+    <View className='items-center px-4 mb-14 '>
+      <View className='flex-row gap-3 items-start'>
+        <View className='justify-center items-center flex-row flex-1'>
+            <View className='h-[46px] w-[46px] rounded-lg border border-secondary justify-center items-center'>
+                <Image 
+                    source={{ uri: avatar }}
+                    className='w-full h-full rounded-lg'
+                    resizeMode='cover'
+                />
+            </View>
+            <View className='justify-center flex-1 ml-3 gap-y-1'>
+              <Text className='text-white font-psemibold text-sm' numberOfLines={1}>
+                {title}
+              </Text>
+              <Text className='text-xs text-gray-100 font-pregular' numberOfLines={1}>
+                { username }
+              </Text>
+            </View>
+        </View>
+        <View className='pt-2'>
+          <Image 
+            source={icons.menu}
+            className='w-5 h-5'
+            resizeMode='contain'
+          />
+        </View>
+      </View>     
+
+      { play ? (
+        <Video
+          source={{ uri: video }}
+          className='w-full h-60 rounded-xl mt-3'
+          resizeMode={ResizeMode.CONTAIN}
+          useNativeControls
+          shouldPlay
+          onPlaybackStatusUpdate={(status) => {
+            if (status.didJustFinish) {
+              setPlay(false);   // brings back the view of the videoCard to the thumbnail when video is complete playing
+            }
+          }}
+        />
+      ) : (
+        <TouchableOpacity
+          activeOpacity={0.7}
+          onPress={() => setPlay(true) }  
+          className='w-full h-60 rounded-xl mt-3 relative justify-center items-center'
+        >
+          {/* { console.log({uri: thumbnail }.uri)} */}
+          <Image 
+            source={{ uri: thumbnail }}
+            className='w-full h-full rounded-xl mt-3'
+            resizeMode='cover'
+          />
+          <Image
+            source={icons.play}
+            className='w-12 h-12 absolute'
+            resizeMode='contain'
+          />
+        </TouchableOpacity>
+      )
+      }
+
+    </View>
+  )
+}
+
+export default VideoCard
