cc6d474665172434681f11ccabe96d05a2ce469e
Vedesh Padal <vpadal04@gmail.com>
2024-05-06 07:45:20 +0530

    Create a post screen complete

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -8,6 +8,7 @@ import EmptyState from '../../components/EmptyState'
 import { getAllPosts, getLatestPosts } from '../../lib/appwrite'
 import useAppwrite from '../../lib/useAppwrite'
 import VideoCard from '../../components/VideoCard'
+import { useGlobalContext } from '../../context/GlobalProvider'
 
 const Home = () => {
 
@@ -15,6 +16,8 @@ const Home = () => {
   const { data: latestPosts } = useAppwrite(getLatestPosts);
 
   const [refreshing, setRefreshing] = useState(false);
+
+  const { user, setUser, setIsLogged } = useGlobalContext();
 
   const onRefresh = async () => {
     setRefreshing(true);
@@ -42,10 +45,10 @@ const Home = () => {
             <View className='justify-between items-start flex-row mb-6'>
               <View>
                 <Text className='font-pmedium text-sm text-gray-100'>
-                  Welcome Back
+                  Welcome back,
                 </Text>
                 <Text className='text-2xl font-psemibold text-white'>
-                  Vedesh Padal
+                  { user?.username }
                 </Text>
               </View>
               <View className='mt-1.5'>
